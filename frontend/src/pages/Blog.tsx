889a859ebdcb808299ba5bc20e1ffe758ab23f13
Chayan Sarkar <131506479+Chayan0099@users.noreply.github.com>
2024-09-29 20:59:37 +0530

    working on blog page

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,11 +1,36 @@
+import axios from "axios";
 import Topbar from "../components/Topbar";
+import { useNavigate } from "react-router-dom";
+type blogtype = {
+    title:string,
+    content:string
+}
 
-function Blog (){
-    return <div>
+async function Blog (){
+    const navigate = useNavigate(); 
+    const token = localStorage.getItem('token'); 
+    const {data} = await axios.get('https://blog-post.chayansarkar2003.workers.dev/api/v1/blog', {
+        headers:{
+            authorization: token
+        }
+    })
+    const blogs = data.blogs; 
+    if(!token) {
+        navigate('/signup')
+    } else { 
+        return <div>
         <Topbar publish={false}></Topbar>
         <div>
-        blog
+            {blogs.map((blog:blogtype) => {<SingleBlog title={blog.title} content={blog.content}></SingleBlog>})}
         </div>
+    </div>
+    }
+}
+
+function SingleBlog({title, content}:{title:string, content:string}) {
+    return <div>
+        <div>{title}</div>
+        <div>{content}</div>
     </div>
 }
 
