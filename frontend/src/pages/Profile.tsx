f05cc6631f223f4329263a4830aac32c54fa26d9
Chayan Sarkar <131506479+Chayan0099@users.noreply.github.com>
2024-10-19 10:09:35 +0530

    profile page loading data from backend

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
+import { BlogType } from "./Blog";
+import axios from "axios";
+
+type Arrayof<T> = T[]; 
+
 function Profile(){
+    const [blogs, setBlogs] = useState<Arrayof<BlogType>>();
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [count, setCount] = useState<number>(); 
+
+    const token = localStorage.getItem('token');
+    useEffect(() => {
+        axios.get('https://blog-post.chayansarkar2003.workers.dev/api/v1/user/getinfo',{
+            headers:{
+                'Authorization':token
+            }
+        }).then((res) => {
+            setName(res.data.info.name);
+            setEmail(res.data.info.email);
+        })
+        axios.get('https://blog-post.chayansarkar2003.workers.dev/api/v1/blog/myblogs',{
+            headers:{
+                'Authorization':token
+            }
+        }).then((res) => {
+            setBlogs(res.data.blogs); 
+            setCount(res.data.blogs.length);
+        })
+    },[])
     return <div> 
     <div className="flex justify-center mt-10">
         <div className="p-10 text-2xl font-serif border max-w-screen-md rounded-lg shadow">
-            <div>Name : Chayan Sarkar </div>
-            <div>Email : Chayan sarkar @email.comfasdfsfasdfadsf</div>
-            <div>Blogs : 69</div>
+            <div>Name : {name} </div>
+            <div>Email : {email}</div>
+            <div>Blogs : {count}</div>
         </div>
     </div>
         <div className="text-center my-10 text-3xl font-bold font-serif">
